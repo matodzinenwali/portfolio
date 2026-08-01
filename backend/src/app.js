0bb1c9552a808cfcd528b8ab4fd03785c13ce0b9
Matodzi <matodzinenwali4@gmail.com>
2026-08-01 05:50:08 +0200

    feat: add achievements routes and update login response message

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import projectRoutes from "./routes/projectRoutes.js";
 import authRoutes from './routes/authRoutes.js';
+import achievementsRoutes from "./routes/achievementsRoutes.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/achievements', achievementsRoutes);
 
 //error-handling middlewarre - every controller`s next(err) lands here
 app.use((err, req, res, next) => {
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,7 +4,7 @@ export async function login(req, res, next) {
     try {
         const { email, password } = req.body;
         const token = await authService.login(email, password);
-        res.status(200).json({ token });
+        res.status(200).json({ message: "log in successful", token });
     } catch (err) {
         next(err);
     }
