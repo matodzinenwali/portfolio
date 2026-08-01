3ec319f9b2304270c709e9267eb21ba546be15ae
Matodzi <matodzinenwali4@gmail.com>
2026-08-01 05:50:33 +0200

    feat: fix userId reference in Achievements schema and add achievements routes

--- backend/src/models/Achievements.js
+++ backend/src/models/Achievements.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const AchievementsSchema = new mongoose.Schema(
     {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
         title: { type: String, required: true },
         issuer: { type: String, required: true },
         type: { type: String, enum: ["award, certification"], required: true },
---
+++ backend/src/routes/achievementsRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getAchievements, getAchievement, createAchievement } from "../controllers/achievementsController.js";
+import { requireAuth } from "../middleware/authMiddleware.js"
+
+const router = Router();
+
+router.get("/", getAchievements);
+router.get("/:id", getAchievement);
+router.post("/", requireAuth, createAchievement);
+
+export default router;
